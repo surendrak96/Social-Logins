becbe9db66c4311879877a2acb460e59046be148
surendra <surendrak96@gmail.com>
2016-08-14 13:38:03 +0530

    committed

--- G+ Login/config.php
+++ G+ Login/config.php
@@ -3,15 +3,15 @@ session_start();
 include_once("src/Google_Client.php");
 include_once("src/contrib/Google_Oauth2Service.php");
 ######### edit details ##########
-$clientId = '964408006817-mgkprqcbi8j7958qf29kt6o1oche5qd3.apps.googleusercontent.com'; //Google CLIENT ID
-$clientSecret = 'sr3Srg2CVjIMvcku1V1A-gPr'; //Google CLIENT SECRET
-$redirectUrl = 'http://localhost/login-with-google-using-php/';  //return url (url to script)
-$homeUrl = 'http://localhost/login-with-google-using-php/';  //return to home
+$clientId = '96440weweqe8006817-mgkprqcbi8j7958qf29kt6o1oche5qd3.apps.googleusercontent.com'; //Google CLIENT ID
+$clientSecret = 'sr3Ssrg2CVjIMvcku1V1A-gPr'; //Google CLIENT SECRET
+$redirectUrl = 'http://localhost/googlelogin/';  //return url (url to script)
+$homeUrl = 'http://localhost/googlelogin/';  //return to home
 
 ##################################
 
 $gClient = new Google_Client();
-$gClient->setApplicationName('ielanat');
+$gClient->setApplicationName('GoogleTest');
 $gClient->setClientId($clientId);
 $gClient->setClientSecret($clientSecret);
 $gClient->setRedirectUri($redirectUrl);
